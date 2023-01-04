e459439905cedc770eb7f1d2f433d046ed8a9bfa
Thibaud Maciuk <thibaud.maciuk@le-campus-numerique.fr>
2023-01-04 15:19:56 +0100

    pdo fin

--- recup.php
+++ recup.php
@@ -1,7 +1,10 @@
 <?php
-$db = new PDO('sqlite:db.sqlite');
-$query = $db->query("SELECT * FROM task");
-$query = $query->fetch();
-$name = $query['Name'];
-echo $name;
+function get_info($nom)
+{
+    $db = new PDO('sqlite:db.sqlite');
+    $query = $db->query("SELECT * FROM task");
+    $query = $query->fetch();
+    $name = $query[''.$nom.''];
+    return $name;
+}
 ?>
