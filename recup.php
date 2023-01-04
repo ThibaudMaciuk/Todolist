b983099e778cfa6295c918bd98dd06b7466abcf6
Thibaud Maciuk <thibaud.maciuk@le-campus-numerique.fr>
2023-01-04 12:36:54 +0100

    test pdo 3

--- recup.php
+++ recup.php
@@ -1,5 +1,6 @@
 <?php
-$db = new PDO('db.sqlite');
-$pdo->exec("");
-
+$db = new PDO('sqlite:db.sqlite');
+$query = $db->query("SELECT * FROM task where id = 1");
+$retour = $retour->fetch();
+echo $retour['name'];
 ?>
